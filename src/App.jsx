83c37cf9869f162db7215a6f732e112f52959b31
Rimjhim Gupta <rimjhimgupta786@gmail.com>
2024-02-12 16:48:27 +0530

    add all files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Components/Navbar/Navbar"
+import Hero from "./Components/Hero-Section/Hero"
+
+const App = () =>{
+  return(
+    <>
+    <Navbar/>
+    <Hero/>
+    </>
+  )
+}
+
+export default App
+
---
+++ src/Components/Hero-Section/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+const Hero = () => {
+  return (
+    
+      <div className="container  ">
+      <div className="row">
+        <div className="col-lg-6 col-md-6 text-center">
+        <h2>Heading for Hero Section</h2>
+        </div>
+        <div className="col-lg-6 col-md-6">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRsgYfMnbmgTb4LbnkMZXJR-0rl_mPx0PYcQ&usqp=CAU" alt=""  />
+          
+        </div>
+        
+      </div>
+    </div>
+    
+  )
+}
+
+export default Hero
